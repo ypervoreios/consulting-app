aa44f6d5d065ccf325391bdceff734c06b68b71c
Administrator <Administrator@APLAN.LOCAL>
2026-05-08 11:11:34 +0300

    Add roles, permissions, UI improvements and seeders

---
+++ database/seeders/RolesAndAdminSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RolesAndAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create roles
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $managerRole = Role::firstOrCreate(['name'=>'manager']);
+        $employeeRole = Role::firstOrCreate(['name' => 'employee']);
+
+        // Create admin user
+        $admin = User::firstOrCreate(
+            ['email' => 'akis@aplan.gr'],
+            [
+                'name' => 'Akis',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        // Assign admin role
+        $admin->assignRole($adminRole);
+    }
+}
